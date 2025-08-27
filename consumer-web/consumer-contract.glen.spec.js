40c15415fdbccc4dbc899cddab9a732874d24e00
glenn222 <glendon.ngo@gmail.com>
2025-08-26 23:42:09 -0400

    Chapter 4 Training - Consumer Driven Contract Testing Activity

---
+++ consumer-web/consumer-contract.glen.spec.js
@@ -0,0 +1,32 @@
+const path = require('path');
+const { fetchMovies } = require('./consumer');
+const { PactV3, MatchersV3 } = require('@pact-foundation/pact');
+
+const provider = new PactV3({
+  dir: path.resolve(process.cwd(), 'pacts'),
+  consumer: 'WebConsumer',
+  provider: 'MoviesAPI',
+});
+const EXPECTED_BODY = { id: 1, name: "Demon Slayer: Infinity Castle", year: 2025 };
+
+describe('Movies Service', () => {
+  describe('When a GET request is made to /movies', () => {
+    test('it should return all movies', async () => {
+      provider
+        .uponReceiving('a request to all movies')
+        .withRequest({
+          method: 'GET',
+          path: '/movies',
+        })
+        .willRespondWith({
+          status: 200,
+          body: MatchersV3.eachLike(EXPECTED_BODY),
+        });
+
+      await provider.executeTest(async mockProvider => {
+        const movies = await fetchMovies(mockProvider.url);
+        expect(movies[0]).toEqual(EXPECTED_BODY);
+      })
+    });
+  });
+});
