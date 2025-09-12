4e48cbbe4060ba91244361ec8d29dc5dc95e878a
glenn222 <glendon.ngo@gmail.com>
2025-09-12 00:54:01 -0400

    weird failing contract test if 2 tests are in same spec

--- graphql/consumer/graphql-client-contract.glen.spec.js
+++ graphql/consumer/graphql-client-contract.glen.spec.js
@@ -28,16 +28,20 @@ const provider = new Pact({
 const EXPECTED_BODY = { id: 1, name: "Demon Slayer: Infinity Castle", year: 2025 };
 
 describe('GraphQL example', () => {
+    // Setup the provider
     beforeAll(() => provider.setup());
 
+    // Generate contract when all tests done
     afterAll(() => provider.finalize());
 
+    // Verify the consumer expectations
     afterEach(() => provider.verify());
 
-    describe('When a query to list all movies on /graphql is made', () => {
+    describe('When a query to /graphql is made', () => {
+        const demonSlayerTestId = 1;
+
         beforeAll(() => {
-            
-            const graphqlQuery = new GraphQLInteraction()
+            const moviesQuery = new GraphQLInteraction()
             .uponReceiving('a movies request')
             .withQuery(
                 `
@@ -59,7 +63,7 @@ describe('GraphQL example', () => {
             .willRespondWith({
                 status: 200,
                 headers: {
-                'Content-Type': 'application/json; charset=utf-8',
+                    'Content-Type': 'application/json; charset=utf-8',
                 },
                 body: {
                     data: {
@@ -68,36 +72,22 @@ describe('GraphQL example', () => {
                 },
             });
 
-            return provider.addInteraction(graphqlQuery);
-
-        })
-
-        test('returns the correct response', async () => {
-            const response = await getMovies();
-            expect(response.movies[0]).toEqual(EXPECTED_BODY);
-        });
-    });
-
-    describe('When a query to a specific movie on /graphql is made', () => {
-        const demonSlayerTestId = 1;
-
-        beforeAll(() => {
-            const graphqlQuery = new GraphQLInteraction()
+            const movieQuery = new GraphQLInteraction()
             .given('Has a movie with specific ID', { id: demonSlayerTestId })
             .uponReceiving('a movie request')
             .withQuery(
                 `
-                    query MovieQuery($movieId: Int!) {
-                        movie(movieId: $movieId) {
-                            id
-                            name
-                            year
-                        }
+                query MovieQuery($movieId: Int!) {
+                    movie(movieId: $movieId) {
+                        id
+                        name
+                        year
                     }
+                }
                 `
             )
             .withOperation('MovieQuery')
-            .withVariables({movieId: 1})
+            .withVariables({movieId: demonSlayerTestId})
             .withRequest({
                 method: 'POST',
                 path: '/graphql',
@@ -117,13 +107,23 @@ describe('GraphQL example', () => {
                     },
                 },
             });
-            return provider.addInteraction(graphqlQuery);
+            
+            provider.addInteraction(moviesQuery);
+            provider.addInteraction(movieQuery);
         })
 
-        test('returns the correct response', async () => {
-            const response = await getMovieById(demonSlayerTestId);
-            expect(response.movie).toEqual(EXPECTED_BODY);
-        });
-    });
+        describe('To get all movies', () => {
+            test('returns the correct response', async () => {
+                const response = await getMovies();
+                expect(response.movies[0]).toEqual(EXPECTED_BODY);
+            });
+        })
 
+        describe('To get a single movie', () => {
+            test('returns the correct response', async () => {
+                const response = await getMovieById(demonSlayerTestId);
+                expect(response.movie).toEqual(EXPECTED_BODY);
+            });
+        })
+    });
 });
