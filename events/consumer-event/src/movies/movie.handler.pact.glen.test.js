27e62ba61b7eb2f9321f17254ca9a5969f1cfec3
glenn222 <glendon.ngo@gmail.com>
2025-09-25 00:22:13 -0400

    Consumer contract test for events

---
+++ events/consumer-event/src/movies/movie.handler.pact.glen.test.js
@@ -0,0 +1,31 @@
+const {
+  MatchersV3,
+  MessageConsumerPact,
+  asynchronousBodyHandler,
+} = require("@pact-foundation/pact");
+const movieEventHandler = require('./movie.handler');
+const { like } = MatchersV3;
+const path = require("path");
+
+describe("Kafka handler", () => {
+  const messagePact = new MessageConsumerPact({
+    consumer: "ConsumerEvent",
+    provider: "EventProducer",
+    dir: path.resolve(process.cwd(), "pacts"),
+    logLevel: "info",
+  })
+
+  it("accepts a movie event", () => {
+    return messagePact
+    .expectsToReceive("a movie add event")
+    .withContent({
+      name: like("The World's End"),
+      year: like("2013")
+    })
+    .withMetadata({
+      "contentType": "application/json",
+      "kafka_topic": "movies",
+    })
+    .verify(asynchronousBodyHandler(movieEventHandler));
+  });
+});
